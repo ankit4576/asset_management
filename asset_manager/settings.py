cd7f1ef723d16ad198a1a5b1108aee2912d5f5ec
Ankit Kumar <00devloper@gmail.com>
2025-11-24 08:35:00 +0530

    Fix accounts app path in INSTALLED_APPS for deployment

--- asset_manager/settings.py
+++ asset_manager/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'asset_manager.accounts',
 ]
 
 MIDDLEWARE = [
